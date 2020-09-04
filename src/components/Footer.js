610ae4af1be6f6ecfafb19dac711e65572a1971a
Abhishek Shetty <abhishekramesh.shetty@nentgroup.com>
2020-09-04 18:03:22 +0200

    Add google analytics

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -126,6 +126,7 @@ const Footer = class extends React.Component {
                   NetlifyCMS
                 </a>
               </span>
+              <a href="javascript:gaOptout();">Deactivate Google Analytics</a>
             </div>
           </div>
         </div>
