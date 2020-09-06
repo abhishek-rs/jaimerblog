a999c644f6a9065b9858873843fa9de972d9793b
Abhishek Shetty <abhishekramesh.shetty@nentgroup.com>
2020-09-06 14:29:10 +0200

    Enabling tag manager, fixing an issue with logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,14 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: `${process.env.GATSBY_GA_TRACKING_ID}`,
+        includeInDevelopment: false,
+        defaultDataLayer: { platform: 'gatsby' },
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
@@ -16,21 +24,7 @@ module.exports = {
         // Setting this parameter is also optional
         respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
-        // Enables Google Optimize Experiment ID
-        experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'example.com',
+        cookieDomain: 'jaimeellenberger.com',
       },
     },
     'gatsby-plugin-react-helmet',
