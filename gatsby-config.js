d327100b6f1c4d96f5d92e16ce4ea0655e47228c
Abhishek Shetty <abhishekramesh.shetty@nentgroup.com>
2020-09-06 14:42:06 +0200

    Final ga tracking fix -> moving to gatsby-gtag plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,26 +5,25 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: 'gatsby-plugin-google-tagmanager',
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        id: `${process.env.GATSBY_GA_TRACKING_ID}`,
-        includeInDevelopment: false,
-        defaultDataLayer: { platform: 'gatsby' },
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: `${process.env.GATSBY_GA_TRACKING_ID}`,
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        cookieDomain: 'jaimeellenberger.com',
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          `${process.env.GATSBY_GA_TRACKING_ID}`, // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
       },
     },
     'gatsby-plugin-react-helmet',
